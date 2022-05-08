aa0d3b22309736d78c3e92b63bb6e5d7a797af71
colinpaint <88828347+colinpaint@users.noreply.github.com>
2022-05-08 13:41:07 +0100

    fiddle

--- compiler/test.pas
+++ compiler/test.pas
@@ -1,13 +1,21 @@
 program test (output);
 
+type
+  upperCase = array [char] of char;
+
 var
   i: integer;
   j: integer;
+  uc: upperCase;
 
 begin
+  uc[64] := 'a';
   for i := 2 to 20 do
     begin
     j := 3 * i;
     Writeln ('hello colin', i, j, i+j);
     end;
+
+  for i := 0 to 255 do
+    Writeln ('convert', uc[i]);
 end.
