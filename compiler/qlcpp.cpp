e2c374e09bcabd0936d1feba578371cc99cb8b97
colinpaint <88828347+colinpaint@users.noreply.github.com>
2022-05-18 16:30:41 +0100

    Update qlcpp.cpp

--- compiler/qlcpp.cpp
+++ compiler/qlcpp.cpp
@@ -81,7 +81,7 @@ private:
     }
   //}}}
   //{{{
-  int getInt() {
+  size_t getSection() {
 
     int value = 0;
     while (true) {
@@ -94,17 +94,17 @@ private:
     }
   //}}}
   //{{{
-  int getHex() {
+  uint32_t getHex() {
 
-    int value = 0;
+    uint32_t value = 0;
     while (true) {
       char ch = getCh();
       if ((ch >= '0') && (ch <= '9'))
-        value = (value * 16) + (ch - '0');
+        value = (value << 4) + (ch - '0');
       else if ((ch >= 'A') && (ch <= 'F'))
-        value = (value * 16) + ((ch - 'A') + 10);
+        value = (value << 4) + ((ch - 'A') + 10);
       else if ((ch >= 'a') && (ch <= 'f'))
-        value = (value * 16) + ((ch - 'a') + 10);
+        value = (value << 4) + ((ch - 'a') + 10);
       else
         return value;
       }
@@ -129,13 +129,13 @@ private:
       // maybe section abase address
       char ch = getCh();
       if (ch == 'o') {
-        int section = getInt();
-        int address = getHex();
+        size_t section = getSection();
+        uint32_t address = getHex();
         if ((section >= 0) && (section <= 15))
           mSectionBaseAddress[section] = address;
 
         if (kSwitchDebug)
-          printf ("processSwitch section %s sectionNum:%2d address:%6x\n", token.c_str(), section, address);
+          printf ("processSwitch section %s sectionNum:%2d address:%6x\n", token.c_str(), (int)section, address);
         }
       else
         printf ("processSwitch - unrecognised switch %s\n", token.c_str());
@@ -161,7 +161,7 @@ public:
   cObjRecord() {}
   virtual ~cObjRecord() = default;
 
-  enum eRecordType { eNone, eModule, eESD, eText, eEOM };
+  enum eRecordType { eNone, eId, eESD, eObjectText, eEnd };
 
   eRecordType getType() { return mType; }
   //{{{
@@ -172,13 +172,15 @@ public:
 
     mLength = stream.get();
     uint8_t b = stream.get();
+
     if ((b > '0') && (b <= '4')) {
+      // recognised record type
       mType = (eRecordType)(b - uint8_t('0'));
-      if (mType < eEOM)
+      if (mType < eEnd)
         for (size_t i = 0; i < mLength-1; i++)
           mBlock[i] = stream.get();
 
-      return mType == eEOM;
+      return mType == eEnd;
       }
 
     else {
@@ -187,17 +189,39 @@ public:
       }
     }
   //}}}
+
+  //{{{
+  uint8_t getByte() {
+
+    if (mBlockIndex >= mLength) {
+      printf ("cObjRecord::getByte past end of record data\n");
+      return 0;
+      }
+
+    return mBlock[mBlockIndex++];
+    }
+  //}}}
+  //{{{
+  uint32_t getInt() {
+
+    uint32_t value = 0;
+    for (int i = 0; i < 4; i++)
+      value = (value << 8) + getByte();
+
+    return value;
+    }
+  //}}}
   //{{{
   string getSymbolName() {
 
     string name;
-    name.resize (kMaxSymbolNameLength);
+    //name.resize (kMaxSymbolNameLength);
 
     for (int i = 0; i < kMaxSymbolNameLength; i++) {
-      char ch = getByte();
-      if (ch == ' ')
+      char byte = getByte();
+      if (byte == ' ')
         break;
-      name = name + ch;
+      name = name + byte;
       }
 
     return name;
@@ -213,8 +237,8 @@ public:
   //{{{
   void dump() {
 
-    if (mType == eEOM)
-      printf ("EOM\n");
+    if (mType == eEnd)
+      printf ("end\n");
 
     else {
       printf ("len:%d type:%d\n", (int)mLength, (int)mType);
@@ -235,23 +259,12 @@ public:
   //}}}
 
 private:
-  //{{{
-  uint8_t getByte() {
-
-    if (mBlockIndex >= mLength) {
-      printf ("cObjRecord::getByte past end of record data\n");
-      return 0;
-      }
-
-    return mBlock[mBlockIndex++];
-    }
-  //}}}
 
   uint8_t mLength = 0;
   eRecordType mType = eNone;
 
   uint8_t mBlockIndex = 0;
-  array <uint8_t,255> mBlock = {0};
+  array <uint8_t,256> mBlock = {0};
   };
 //}}}
 //{{{
@@ -302,15 +315,91 @@ void processObjFile (const string& line, vector <string>& objFiles) {
   if (kObjFileDebug)
     printf ("processLine %s\n", line.c_str());
 
-  // look for trailing comma
-  size_t foundComma = line.find (',');
+  // find any trailing comma
+  size_t foundTerminator = line.find (',');
+  if (foundTerminator == string::npos) // no comma, find any trailing cr in linux file i/o
+    foundTerminator = line.find ('\r');
 
   // look for extension dot, assumes only one dot, !!! could search backwards !!!
   size_t foundDot = line.find ('.');
   if (foundDot == string::npos) // no dot, use default ext
-    objFiles.push_back (line.substr (0, foundComma) + ".ro");
+    objFiles.push_back (line.substr (0, foundTerminator) + ".ro");
   else
-    objFiles.push_back (line.substr (0, foundComma));
+    objFiles.push_back (line.substr (0, foundTerminator));
+  }
+//}}}
+
+//{{{
+void processESD (cObjRecord& objRecord, cSwitches& switches, const cLink& link) {
+// process External Symbol Definition record
+
+  uint8_t byte = objRecord.getByte();
+  size_t section = byte % 16;
+  uint8_t esdType = byte / 16;
+
+  switch (esdType) {
+    //{{{
+    case 0: { // absolute section
+      uint32_t size = objRecord.getInt();
+      uint32_t start = objRecord.getInt();
+      printf ("Absolute %08x %08x\n", size, start);
+      break;
+      }
+    //}}}
+    //{{{
+    case 1: { // common section xx
+      string commonSymbolName = objRecord.getSymbolName();
+      uint32_t size = objRecord.getInt();
+      printf ("common data - section:%2d %s size:%08x\n", (int)section, commonSymbolName.c_str(), size);
+      break;
+      }
+    //}}}
+    case 2:        // standard relocatable section xx
+    //{{{
+    case 3: { // short address relocatable section xx
+      uint32_t size = objRecord.getInt();
+      printf ("section def - section:%2d size:%08x\n", (int)section, size);
+      break;
+      }
+    //}}}
+    case 4:        // external symbol defintion in relocatble section xx
+    //{{{
+    case 5: { // external symbol defintion in absolute section
+      string xdefSymbolName = objRecord.getSymbolName();
+      uint32_t address = objRecord.getInt();
+      printf ("symbol xdef - section:%2d %s address:%08x\n", (int)section, xdefSymbolName.c_str(), address);
+      }
+    //}}}
+    case 6:        // external symbol reference to section xx
+    //{{{
+    case 7: { // external symbol reference to any section
+      string xrefSymbolName = objRecord.getSymbolName();
+      printf ("symbol xref - section:%2d %s\n", (int)section, xrefSymbolName.c_str());
+      break;
+      }
+    //}}}
+    case 8:        // command line address in section
+    //{{{
+    case 9:  {// command line address in absolute section
+      printf ("clAddress\n");
+      for (int i = 0; i < 5; i++)
+        objRecord.getByte();
+      break;
+      }
+    //}}}
+    //{{{
+    case 10: {// command line address in a a common section in section xx
+      printf ("clAddr common\n");
+      for (int i = 0; i < 15; i++)
+        objRecord.getByte();
+      break;
+      }
+    //}}}
+    //{{{
+    default:
+      printf ("unknown esdType %d\n", esdType);
+    //}}}
+    }
   }
 //}}}
 
@@ -331,16 +420,17 @@ void pass1File (const string& fileName, cSwitches& switches, const cLink& link) 
         objRecord.dump();
 
       switch (objRecord.getType()) {
-        case cObjRecord::eModule: {
+        case cObjRecord::eId: {
           string modName = objRecord.processModuleId();
           printf ("ModuleId - modName:%s\n", modName.c_str());
           break;
           }
         case cObjRecord::eESD:
+          processESD (objRecord, switches, link);
           break;
-        case cObjRecord::eText:
+        case cObjRecord::eObjectText:
           break;
-        case cObjRecord::eEOM:
+        case cObjRecord::eEnd:
           break;
         default:
           printf ("unrecognised objRecord %d\n", objRecord.getType());
@@ -369,13 +459,13 @@ void pass2File (const string& fileName, cSwitches& switches, const cLink& link) 
         objRecord.dump();
 
       switch (objRecord.getType()) {
-        case cObjRecord::eModule:
+        case cObjRecord::eId:
           break;
         case cObjRecord::eESD:
           break;
-        case cObjRecord::eText:
+        case cObjRecord::eObjectText:
           break;
-        case cObjRecord::eEOM:
+        case cObjRecord::eEnd:
           break;
         default:
           break;
